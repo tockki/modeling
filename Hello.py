4adeeccfb080343c988512a17cb1e9cc87cd7b03
joon <joon_nam@yahoo.com>
2023-10-16 08:25:46 -0400

    Uploading Hello.py

---
+++ Hello.py
@@ -0,0 +1,42 @@
+print ('hello')
+list=['grapes','apple', 'pear', 'peach', 'plum' ]
+print (list[0])
+import numpy as np
+print(np.sort(list))
+
+#apple, grapes, peach, pear plum
+
+
+# booleans -> True or False
+# print(list)
+# print('grapes' in list)  #-> True
+
+# comparison operator -> compare 2 elements and return a boolean
+# <, >, ==, <=, >=, etc
+print(23 > 56)  # -> False
+print(4 == 7)
+
+# hey tell us your age
+# if person >= 18 and show id -> True
+#     let him or her in 
+# else: 
+    
+# hey tell us your age
+# if customer has logged in or Guest:
+#     let him or her in 
+# else: 
+    
+
+#checking for equality
+56 == 56 
+56 != 56   #-> boolean value -> False
+
+# logical operator -> check for conditions
+# -> and, or, not, etc. 
+4 == 7 or  23 > 56 or 6>=3
+
+
+# branching - if, elif (else if), else statements
+
+
+
